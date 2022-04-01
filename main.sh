9174901059837b0cc034fce4f79953249e4b2bad
WaxySteelWorm <64628149+WaxySteelWorm@users.noreply.github.com>
2022-04-01 09:47:43 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -16,7 +16,10 @@ vNet="tor-bridge-vnet-$randomIdentifier"
 nsg="tor-bridge-nsg-$randomIdentifier"
 subnet="tor-bridge-single-dual-stack-subnet-$randomIdentifier"
 subnetAddressPrefixes="10.0.0.0/24 fd00:db8:deca:deed::/64"
-
+ipV4PublicIp="tor-ipV4-public-ip-address-$randomIdentifier"
+ipV6PublicIp="tor--ipV6-public-ip-address-$randomIdentifier"
+sku="BASIC"
+allocationMethod="dynamic"
 #########################################################################3
 
 echo "Using resource group $resourceGroup with login: $login, password: $password..."
@@ -35,3 +38,11 @@ az network nsg create --name $nsg --resource-group $resourceGroup --location "$l
 # Create a single dual stack subnet with IPv4 and IPv6 addresses
 echo "Creating $subnet"
 az network vnet subnet create --name $subnet --resource-group $resourceGroup --vnet-name $vNet --address-prefixes $subnetAddressPrefixes --network-security-group $nsg
+
+# Create an IPV4 IP address
+echo "Creating $ipV4PublicIp"
+az network public-ip create --name $ipV4PublicIp --resource-group $resourceGroup --location "$location" --sku $sku --allocation-method $allocationMethod --version IPv4
+
+# Create an IPV6 IP address
+echo "Creating $ipV6PublicIp"
+az network public-ip create --name $ipV6PublicIp --resource-group $resourceGroup --location "$location" --sku $sku --allocation-method $allocationMethod --version IPv6
