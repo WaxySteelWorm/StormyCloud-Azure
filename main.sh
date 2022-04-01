30a48456694f7c442e61533c8db81904872644e6
WaxySteelWorm <64628149+WaxySteelWorm@users.noreply.github.com>
2022-04-01 09:43:39 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -13,8 +13,11 @@ vNetAddressPrefixes="10.0.0.0/16 fd00:db8:deca::/48"
 location="East US"
 resourceGroup="tor-bridge-rg-$randomIdentifier"
 vNet="tor-bridge-vnet-$randomIdentifier"
+nsg="tor-bridge-nsg-$randomIdentifier"
+subnet="tor-bridge-single-dual-stack-subnet-$randomIdentifier"
+subnetAddressPrefixes="10.0.0.0/24 fd00:db8:deca:deed::/64"
 
-
+#########################################################################3
 
 echo "Using resource group $resourceGroup with login: $login, password: $password..."
 
@@ -24,3 +27,11 @@ az group create --name $resourceGroup --location "$location"
 
 # echo "Creating $vNet"
 az network vnet create --name $vNet --resource-group $resourceGroup --location "$location" --address-prefixes $vNetAddressPrefixes
+
+# Create network security group
+echo "Creating $nsg"
+az network nsg create --name $nsg --resource-group $resourceGroup --location "$location"
+
+# Create a single dual stack subnet with IPv4 and IPv6 addresses
+echo "Creating $subnet"
+az network vnet subnet create --name $subnet --resource-group $resourceGroup --vnet-name $vNet --address-prefixes $subnetAddressPrefixes --network-security-group $nsg
